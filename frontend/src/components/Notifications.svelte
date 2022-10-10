8845261b2e29ffa3e9b853e5c616907133b4ef04
seekers <seekersMan@qq.com>
2022-10-10 09:41:44 +0800

    fix:Modify the location of occurrence

--- frontend/src/components/Notifications.svelte
+++ frontend/src/components/Notifications.svelte
@@ -1,25 +1,24 @@
 <script>
-  import { ToastNotification } from "carbon-components-svelte";
-  import { notificationStore } from "../store/notification";
+    import { ToastNotification } from 'carbon-components-svelte';
+    import { notificationStore } from '../store/notification';
 </script>
 
-<style>
-  .notification-position {
-    position: fixed;
-    width: 400px;
-    top: 0;
-    right: 0;
-  }
-</style>
-
 <div class="notification-position">
-  {#each $notificationStore as n}
-    <ToastNotification
-      kind="{n.level}"
-      title=""
-      caption=""
-      subtitle="{n.content}"
-      timeout=7000
-    />
-  {/each}
+    {#each $notificationStore as n}
+        <ToastNotification
+            kind={n.level}
+            title=""
+            caption=""
+            subtitle={n.content}
+            timeout="7000"
+        />
+    {/each}
 </div>
+
+<style>
+    .notification-position {
+        position: fixed;
+        width: 400px;
+        z-index: 99999;
+    }
+</style>
