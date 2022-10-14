409734b422c5a9ea2551af97dd7976cd125c8293
yunkuangao <a317526763@gmail.com>
2022-10-14 11:47:04 +0800

    删掉手动指定image

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,6 @@ ktor {
         localImageName.set("url-random")
         imageTag.set(System.getenv("GITHUB_REF"))
         jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
-        jib.to.image = "jib-image.tar"
         externalRegistry.set(
             io.ktor.plugin.features.DockerImageRegistry.dockerHub(
                 appName = provider { "url-random" },
