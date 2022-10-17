40a3716e56d4539cd48214964f2dd3fbeedbe576
yunkuangao <a317526763@gmail.com>
2022-10-17 17:04:39 +0800

    再次尝试

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,8 +112,8 @@ ktor {
         externalRegistry.set(
             io.ktor.plugin.features.DockerImageRegistry.dockerHub(
                 appName = provider { "url-random" },
-                username = providers.environmentVariable("secrets.DOCKER_HUB_USERNAME"),
-                password = providers.environmentVariable("secrets.DOCKER_HUB_PASSWORD"),
+                username = providers.environmentVariable("DOCKER_HUB_USERNAME"),
+                password = providers.environmentVariable("DOCKER_HUB_PASSWORD"),
             )
         )
     }
